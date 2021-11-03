fe75ee9fe2dbafcde200776cca1dcfdec7654f73
Mitrich89 <sazonkin.v@yandex.ru>
2021-11-03 14:59:42 +0300

    added user verification

--- lab2DB/initialization.cpp
+++ lab2DB/initialization.cpp
@@ -1,5 +1,6 @@
 #include "initialization.h"
 #include "ui_initialization.h"
+#include <QFile>
 
 
 initialization::initialization(QSqlDatabase& db) :
@@ -9,7 +10,9 @@ initialization::initialization(QSqlDatabase& db) :
 {
     ui->setupUi(this);
     file_conn.setFileName("Connections.txt");
-    file_conn.open(QIODevice::Append);
+    file_conn.open(QIODevice::ReadOnly);
+    temp.setFileName("temp.txt");
+    temp.open(QIODevice::Append);
 }
 
 initialization::~initialization()
@@ -19,38 +22,61 @@ initialization::~initialization()
 
 void initialization::on_pushButton_clicked()
 {
+    //проверка на заполнение
     if(ui->ip_Label->text() == "" || ui->password_Label->text() == ""|| ui->name_Label->text() == "" || ui->port_Label->text() == "" || ui->login_Label->text() == ""){
         ui->m_error->setText("Please, fill in all input fields");
     }
     else{
-    time2 = QDateTime::currentDateTime();
-    m_db.setDatabaseName(ui->name_Label->text());
-    m_db.setHostName(ui->ip_Label->text());
-    m_db.setPassword(ui->password_Label->text());
-    m_db.setPort(ui->port_Label->text().toInt());
-    m_db.setUserName(ui->login_Label->text());
+        //проверка на правильный ввод
+        QTextStream out_temp(&temp);
+        QTextStream out_conn(&file_conn);
+        time2 = QDateTime::currentDateTime();
 
-    QTextStream in(&file_conn);
-    QString line = in.readLine();
-    while (!line.isNull()) {
-        process_line(line);
-        line = in.readLine();
-    }
+        bool flag_pass = false;
+        bool new_user = true;
+
+        QTextStream in(&file_conn);
+        QString line = in.readLine();
+        qint64 pos;
+        while (!line.isNull()) {
+            if(line == ui->login_Label->text()){
+                new_user = false;
+                pos = in.pos();
+                line = in.readLine();
+                if(line == ui->password_Label->text()){
+                    flag_pass = true;
+                }
+                else {
+                    line = in.seek(pos);
+                }
+            }
+            line = in.readLine();
+        }
+        if(flag_pass == false && new_user == false)
+            ui->m_error->setText("Incorrect login or password");
+        else{
+
+            m_db.setDatabaseName(ui->name_Label->text());
+            m_db.setHostName(ui->ip_Label->text());
+            m_db.setPassword(ui->password_Label->text());
+            m_db.setPort(ui->port_Label->text().toInt());
+            m_db.setUserName(ui->login_Label->text());
+
+            file_conn.close();
+            file_conn.open(QIODevice::Append);
+            out_conn<< time2.toString("[dd.MM.yy , hh:mm:ss] -> ")
+                       + ui->name_Label->text() + '\n'
+                       + ui->ip_Label->text() + '\n'
+                       + ui->port_Label->text() + '\n'
+                       + ui->login_Label->text() + '\n'
+                       + ui->password_Label->text() + '\n' + '\n';
 
 
-    QTextStream out_conn(&file_conn);
-
-    out_conn<< time2.toString("[dd.MM.yy , hh:mm:ss] -> ")
-               + ui->name_Label->text() + '\n'
-               + ui->ip_Label->text() + '\n'
-               + ui->port_Label->text() + '\n'
-               + ui->login_Label->text() + '\n'
-               + ui->password_Label->text() + '\n' + '\n';
-
-
-    if(m_db.open())
-        accept();
-    else
-        ui->m_error->setText(m_db.lastError().text());
+            if(m_db.open())
+                accept();
+            else
+                ui->m_error->setText(m_db.lastError().text());
+            file_conn.close();
+        }
     }
 }
